39cef5740a4b7e38c2d8050465e70ad47c0d2c5a
KVRDV <s.kovardaev@gmail.com>
2021-03-23 22:48:56 +0300

    proj about

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const projects = document.querySelectorAll('.project');
 const projectAboutOpenButton = document.querySelectorAll('.project-about-open-button');
 const bodyMask = document.querySelector('.body-mask');
 const buttonBack = document.querySelectorAll('.button-back');
+const html = document.querySelector("html");
 
 // Показать все проекты:
 buttonMore.forEach(function (item) {
@@ -40,6 +41,7 @@ projectAboutOpenButton.forEach(function (item) {
     event.preventDefault();    
     item.parentElement.lastElementChild.classList.remove('hidden');
     document.body.style.overflow = 'hidden';
+    html.style.overflow = 'hidden';
     document.body.style.zIndex =-1;
     bodyMask.classList.remove('hidden');   
   });   
@@ -51,6 +53,7 @@ buttonBack.forEach(function (item) {
     event.preventDefault();    
     item.parentElement.parentElement.classList.add('hidden');
     document.body.style.overflow = 'auto';
+    html.style.overflow = 'auto';
     document.body.style.zIndex =0;
     bodyMask.classList.add('hidden');
   });   
